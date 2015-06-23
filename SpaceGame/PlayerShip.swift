522a030e96641e6e87044f3d955285dea4a97280
Pablo Henrique Bertaco <pablo_fonseca91@icloud.com>
2015-06-23 08:38:53 -0300

    + lógica de movimentação básica

--- SpaceGame/PlayerShip.swift
+++ SpaceGame/PlayerShip.swift
@@ -20,6 +20,19 @@ class PlayerShip: Control {
     var shieldPower:Int = 0
     var shieldRecharge:Int = 0
     
+    //Movimentação
+    var needToMove:Bool = false
+    var destination:CGPoint = CGPoint.zeroPoint
+    var rotation:CGFloat = 0
+    var totalRotation:CGFloat = 0
+    
+    //Toques
+    var lastNoTouchTime:Double = 0
+    var touchesArrayCount: Int = 0
+    var lastTouchesArrayCount: Int = 0
+    var firstTouchLocation:CGPoint = CGPoint.zeroPoint
+    var lastTouchLocation:CGPoint = CGPoint.zeroPoint
+    
     init(index:Int, x:Int, y:Int) {
         super.init()
         loadNewShip(index, x: x, y: y)
@@ -38,9 +51,89 @@ class PlayerShip: Control {
         self.shieldPower += Int(playerShipData.bonusShieldPower)
         self.shieldRecharge += Int(playerShipData.bonusShieldRecharge)
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func update(currentTime: NSTimeInterval) {
+        
+        self.lastTouchesArrayCount = self.touchesArrayCount
+        self.touchesArrayCount = Control.touchesArray.count
+        
+        switch(self.touchesArrayCount) {
+            
+            //Zero toques
+        case 0:
+            //Um toque curto
+            if((self.lastTouchesArrayCount == 1) && currentTime - lastNoTouchTime < 1) {
+                self.needToMove = true
+                self.destination = self.firstTouchLocation
+            }
+            self.lastNoTouchTime = currentTime
+            
+            if(self.needToMove){
+                self.setRotationToPoint(self.destination)
+            }
+            
+            break
+            
+            //Um toque
+        case 1:
+            self.firstTouchLocation = (Control.touchesArray[0] as! UITouch).locationInNode(self.parent)
+            
+            self.setRotationToPoint(self.firstTouchLocation)
+            break
+            
+            //Mais de um toque
+        default:
+            
+            self.firstTouchLocation = (Control.touchesArray[0] as! UITouch).locationInNode(self.parent)
+            self.lastTouchLocation = (Control.touchesArray.lastObject as! UITouch).locationInNode(self.parent)
+            
+            self.needToMove = true
+            self.destination = lastTouchLocation
+            
+            self.setRotationToPoint(self.firstTouchLocation)
+            
+            break
+        }
+        
+        if (self.needToMove) {
+            var dX:CGFloat = destination.x - self.position.x
+            var dY:CGFloat = destination.y - self.position.y
+            var distanceToDestination:CGFloat = sqrt((dX * dX) + (dY * dY))
+            
+            
+            if(distanceToDestination < 64) {
+                needToMove = false
+            } else {
+                switch(self.touchesArrayCount) {
+                case 0:
+                    self.physicsBody!.applyForce(CGVector(dx: -sin(self.zRotation) * 100, dy: cos(self.zRotation) * 100))
+                    
+                    break
+                    
+                default:
+                    //aplicar forca em direcao ao destino
+                    self.physicsBody!.applyForce(CGVector(dx: (dX/distanceToDestination) * 100, dy: (dY/distanceToDestination) * 100))
+                    break
+                }
+            }
+        }
+        
+        if(abs(self.physicsBody!.angularVelocity) < CGFloat(M_PI * 2) && (self.needToMove || self.touchesArrayCount > 0)) {
+            self.totalRotation = self.rotation - self.zRotation
+            
+            while(self.totalRotation < -CGFloat(M_PI)) { self.totalRotation += CGFloat(M_PI * 2) }
+            while(self.totalRotation >  CGFloat(M_PI)) { self.totalRotation -= CGFloat(M_PI * 2) }
+            
+            self.physicsBody!.applyAngularImpulse(self.totalRotation *  0.005)
+        }
+    }
+    
+    func setRotationToPoint(point:CGPoint) {
+        self.rotation = CGFloat(M_PI) + CGFloat(-atan2f(Float(self.position.x - point.x), Float(self.position.y - point.y)))
     }
     
     func loadNewShip(index:Int, x:Int, y:Int, loadPhysics:Bool = false) {
@@ -69,7 +162,10 @@ class PlayerShip: Control {
         spriteNode.name = "player"
         
         if(loadPhysics){
+            //Physics Config
             self.physicsBody = SKPhysicsBody(texture: texture, alphaThreshold: 0.7, size: texture.size())
+            self.physicsBody!.angularDamping = 10
+            self.physicsBody!.linearDamping = 5
         }
         
         self.addChild(spriteNode)
