85f1718478319fac9fa20ffc6a54b3a20fad61e1
Uriel <urielbattanoli@hotmail.com>
2015-06-26 16:57:15 -0300

    login e signup

---
+++ SpaceGame/Reachability.swift
@@ -0,0 +1,33 @@
+//
+//  Reachability.swift
+//  SpaceGame
+//
+//  Created by Uriel on 24/06/15.
+//  Copyright (c) 2015 WTFGames. All rights reserved.
+//
+
+import UIKit
+
+class Reachability: NSObject {
+    class func isConnectedToNetwork() -> Bool {
+        var status:Bool = false
+        //dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0)) { () -> Void in
+            let url = NSURL(string: "http://google.com")
+            let request = NSMutableURLRequest(URL: url!)
+            request.HTTPMethod = "HEAD"
+            request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalAndRemoteCacheData
+            request.timeoutInterval = 10.0
+            
+            var response:NSURLResponse?
+            
+            var data = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: nil) as NSData?
+            
+            if let httpResponse = response as? NSHTTPURLResponse {
+                if httpResponse.statusCode == 200 {
+                    status = true
+                }
+            }
+       // }
+        return status
+    }
+}
---
+++ SpaceGame/SocialScene.swift
@@ -0,0 +1,143 @@
+//
+//  SocialScene.swift
+//  SpaceGame
+//
+//  Created by Uriel on 24/06/15.
+//  Copyright (c) 2015 WTFGames. All rights reserved.
+//
+
+import SpriteKit
+import Parse
+import GameKit
+
+class SocialScene: SKScene {
+    
+    var localPlayer = GKLocalPlayer.localPlayer()
+    var arrayFriends = NSMutableArray()
+    
+    override init() {
+        Control.locations = NSMutableArray()
+        super.init(size: Config.sceneSize())
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func didMoveToView(view: SKView) {
+        
+        self.scaleMode = SKSceneScaleMode.AspectFit
+        self.backgroundColor = GameColors.gray
+        self.anchorPoint = CGPoint(x: 0, y: 1)
+        
+        self.addChild(Button(name: "buttonBack", x:81, y:633, xAlign:.left, yAlign:.down))
+        
+        if !(Reachability.isConnectedToNetwork()){
+            let alert = Alert(text: "Oops! An error occurred while connecting to server.")
+            alert.touchesEndedAtButtonOK.addHandler({
+                self.view?.presentScene(HangarScene(), transition: SKTransition.crossFadeWithDuration(1))
+            })
+            self.addChild(alert)
+        }else{
+            if self.authenticateLocalPlayer(){
+                var logou = false
+                if(PFUser.currentUser() != nil){ logou = true}
+                else{
+                    PFUser.logInWithUsernameInBackground(self.localPlayer.displayName, password: "123", block: { (user, error) -> Void in
+                        if let usr = user{
+                            logou = true
+                        }
+                        else if (error != nil){
+                            let user = PFUser()
+                            user.username = self.localPlayer.displayName
+                            user.password = "123"
+                            user["Installation"] = PFInstallation.currentInstallation()
+                            user.signUpInBackgroundWithBlock({ (succeeded, error) -> Void in
+                                println(succeeded)
+                                if succeeded{
+                                    logou = true
+                                }
+                            })
+                        }
+                    })
+                }
+                if logou{
+                    
+                    let query = PFQuery(className: "_User")
+                    query.whereKey("username", equalTo: "Uriel")
+                    query.findObjectsInBackgroundWithBlock({ (objects:[AnyObject]?, error:NSError?) -> Void in
+                        if let e = error{
+                            println(e)
+                        }
+                        else{
+                            let user1 = objects![0] as! PFUser
+                            let user = PFUser.currentUser()
+                            
+                            var array  = user?.relationForKey("friends")//pegar amigos
+                            
+                            array?.addObject(user1)//add amigos
+                            user?.saveInBackgroundWithBlock({ (success, error) -> Void in
+                                if success{
+                                    print("dedado")
+                                }
+                            })
+                            println(array?.description)
+                            //array?.removeObject(user1)//remover amigos
+                        }
+                    })
+                    
+                    
+                    
+                    
+                }
+            }
+            else{
+                // !authenticated
+                
+            }
+        }
+    }
+    
+    func authenticateLocalPlayer()->Bool{
+        var authenticated = false
+        self.localPlayer.authenticateHandler = {(viewController : UIViewController!, error : NSError!) -> Void in
+            
+            if viewController != nil{
+                //method that displays an authentication view when appropriate for your app
+            }
+            else{
+                if self.localPlayer.authenticated {
+                    authenticated = true
+                }
+            }
+        }
+        
+        return true//depois trocar para authenticated
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        Control.touchesBegan(self, touches: touches as! Set<UITouch>)
+    }
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        Control.touchesMoved(self)
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        Control.touchesEnded(self, touches: touches as! Set<UITouch>)
+        for touch in (touches as! Set<UITouch>) {
+            let location = touch.locationInNode(self)
+            
+            if (self.childNodeWithName("buttonBack")!.containsPoint(location)) {
+                self.view?.presentScene(HangarScene(), transition: SKTransition.crossFadeWithDuration(1))
+                return
+            }
+        }
+    }
+    
+    override func touchesCancelled(touches: Set<NSObject>!, withEvent event: UIEvent!) {
+        Control.touchesEnded(self.scene!, touches: touches as! Set<UITouch>)
+    }
+    
+}
